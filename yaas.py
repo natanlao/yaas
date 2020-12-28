0496887c23390a05242713ede59d52356ffa1f7e
Natan Lao <natanlao@users.noreply.github.com>
2020-12-27 17:40:51 -0800

    Add playlist support, show all video formats, improve error handling

--- yaas.py
+++ yaas.py
@@ -1,48 +1,101 @@
-# -*- coding: utf-8 -*-
-import functools
-import os
+from typing import Mapping, Sequence
 
-from flask import flash, Flask, render_template, redirect, request, url_for
+from flask import Flask, jsonify, render_template, redirect, request, url_for
+from jinja2 import StrictUndefined
 import youtube_dl
 
 __version__ = "0.2.0"
 
+
 app = Flask(__name__)
-app.secret_key = os.urandom(24)  # dirty
-app.jinja_env.globals.update({
-    'youtubedl_version': youtube_dl.version.__version__,
-    'yaas_version': __version__
-})
-ydl = youtube_dl.YoutubeDL({'skip_download': True})
+app.jinja_env.undefined = StrictUndefined
+
+ydl = youtube_dl.YoutubeDL({
+    'no_color': True,
+    'skip_download': True
+})  # TODO: Set logger, cachedir?
 ydl.add_default_info_extractors()
 
 
 @app.route('/')
-def serve_index():
-    return render_template("index.html")
+def index():
+    return render_template('_base.html')
 
 
-@app.route("/video", methods=['POST'])
-def get_video():
+@app.route('/video', methods=['POST'])
+def fetch_redirect():
+    if request.form['url']:
+        return redirect(url_for('fetch', url=request.form['url']))
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/fetch')
+def fetch():
     try:
-        url = get_video_info(request.form['url'])
-        assert url
+        url = request.args['url']
+    except KeyError:
+        return redirect(url_for('index'))
+
+    try:
+        videos = get_video_info(url)
     except youtube_dl.utils.DownloadError as e:
-        if "not a valid URL" in str(e):
-            flash("The provided URL is not valid.")
-        elif "Unsupported URL" in str(e):
-            flash("The provided URL is not supported.")
-        return redirect(url_for('serve_index'))
-    except:
-        flash("There was an error.")
-        return redirect(url_for('serve_index'))
-    return redirect(url)
+        return render_template('error.html', error=parse_err(e))
+    else:
+        return render_template('video.html', videos=videos)
 
 
-@functools.lru_cache(maxsize=None)
-def get_video_info(url):
-    return ydl.extract_info(url)['url']
+def get_video_info(url: str) -> Sequence[Mapping]:
+    """
+    >>> get_video_info(...)
+    {}
+    >>> get_video_info(...)
+    """
+    info = ydl.extract_info(url)
+    if info.get('_type') == 'playlist':
+        return info['entries']
+    else:
+        return [info]
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def parse_err(err: youtube_dl.utils.DownloadError) -> str:
+    msg = err.args[0]
+    log_prefix = 'ERROR: '
+    invalid_url = [
+        'is not a valid URL',
+        'Name or service not known',
+        'URLError'
+    ]
+    if any(err in msg for err in invalid_url):
+        return 'The provided URL is invalid.'
+    elif 'Unsupported URL' in msg:
+        return 'The provided URL is not supported by youtube-dl.'
+    elif msg.startswith(log_prefix):
+        return f'Unknown error: {msg[len(log_prefix):]}'
+    else:
+        return f'Unknown error: {msg!r}'
+
+
+
+# https://stackoverflow.com/a/1094933
+def human_filesize(num: int, suffix: str = 'B'):
+    for unit in ('', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi'):
+        if abs(num) < 1024.0:
+            return f'{num:3.1f} {unit}{suffix}'
+        num /= 1024.0
+    return f'{num:.1f} Yi{suffix}'
+
+
+app.jinja_env.filters['human_filesize'] = human_filesize
+
+
+@app.context_processor
+def versions() -> Mapping[str, str]:
+    return {
+        'youtubedl_version': youtube_dl.version.__version__,
+        'yaas_version': __version__
+    }
+
+
+# TODO: Error handlers
+
